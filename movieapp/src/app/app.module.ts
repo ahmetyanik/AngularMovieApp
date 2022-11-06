06333b42279686b89d824a430255a1d9daeefa32
ahmetyanik <ahmetyank4242@gmail.com>
2022-11-06 12:55:21 +0100

    components are created

--- movieapp/src/app/app.module.ts
+++ movieapp/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CategoryComponent } from './category/category.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MovieComponent } from './movies/movie/movie.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [ // component
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    CategoryComponent,
+    MoviesComponent,
+    MovieComponent,
+    MovieDetailsComponent,
+    FooterComponent
   ],
   imports: [   // module
     BrowserModule

454185be23773b74dbfd745f4f81c2a97a4f2b55
ahmetyanik <ahmetyank4242@gmail.com>
2022-11-06 12:48:33 +0100

    Basic infos

---
+++ movieapp/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+  declarations: [ // component
+    AppComponent,
+    NavbarComponent
+  ],
+  imports: [   // module
+    BrowserModule
+  ],
+  providers: [],  // services
+  bootstrap: [AppComponent]  // starter component
+})
+export class AppModule { }
